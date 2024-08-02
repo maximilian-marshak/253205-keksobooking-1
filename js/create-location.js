fec527933ba32cfe3f2f06b6c37ea4c257316c49
Maksim Rusakov <maximilian.workfrontend@gmail.com>
2024-08-02 19:21:43 +0300

    fix: add new module constants. Remove unused code

--- js/create-location.js
+++ js/create-location.js
@@ -1,5 +1,5 @@
 import {getRandomFloatNumber} from './util.js';
-import {MIN_LATITUDE, MIN_LONGITUDE, MAX_LATITUDE, MAX_LONGITUDE} from './create-offer.js';
+import {MIN_LATITUDE, MIN_LONGITUDE, MAX_LATITUDE, MAX_LONGITUDE} from './constants.js';
 
 const createLocation = () => ({
   lat: getRandomFloatNumber(MIN_LATITUDE, MAX_LATITUDE),
--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-import './create-author.js';
-import './create-location.js';
-import './create-offer.js';
-import './util.js';
 import {similiarAnnounce} from './data.js';
 
 similiarAnnounce();
